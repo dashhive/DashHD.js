a34712c848f4e078eeacf1d7316149c69523d2cf
AJ ONeal <coolaj86@gmail.com>
2022-12-07 00:09:26 -0700

    feat: prove equivalent of derive(path) and deriveChild(pathPart)...

--- bin/seed-to-wif.js
+++ bin/seed-to-wif.js
@@ -16,12 +16,17 @@ async function main() {
 
   let [seedPath] = args;
   if (!seedPath) {
-    console.error("Usage: seed-to-wif <./seed.hex> [account] [change] [index]");
+    console.error("");
+    console.error(
+      "Usage: seed-to-wif <./seed.hex> [account] [direction] [index]",
+    );
+    console.error("");
     process.exit(1);
   }
-  let account = 0;
-  let direction = 0;
-  let index = 0;
+
+  let account = parseInt(args[1], 10) || 0;
+  let direction = parseInt(args[2], 10) || 0;
+  let index = parseInt(args[3], 10) || 0;
 
   let txt = await Fs.readFile(seedPath, "utf8");
   let seedHex = txt.trim();
@@ -29,24 +34,74 @@ async function main() {
 
   let privateRoot = HdKey.fromMasterSeed(seedBuf);
 
-  let childKey = privateRoot.derive(
+  // a "wallet" is the path up to `m/44'/${coinType}'/${account}'`
+  // the "addresses" are the paths down from `${direction}/${index}`
+  let referenceKey = privateRoot.derive(
     `m/44'/${coinType}'/${account}'/${direction}/${index}`,
   );
-
-  let wif = await b58c.encode({
-    privateKey: childKey.privateKey.toString("hex"),
+  let wif1 = await b58c.encode({
+    privateKey: referenceKey.privateKey.toString("hex"),
     compressed: true,
   });
 
-  console.info(`WIF:`, wif);
+  referenceKey = privateRoot
+    .deriveChild(44 + HdKey.HARDENED_OFFSET)
+    .deriveChild(coinType + HdKey.HARDENED_OFFSET)
+    .deriveChild(account + HdKey.HARDENED_OFFSET)
+    .deriveChild(direction)
+    .deriveChild(index);
+  let wif2 = await b58c.encode({
+    privateKey: referenceKey.privateKey.toString("hex"),
+    compressed: true,
+  });
+
+  let directionRoot = privateRoot.derive(
+    `m/44'/${coinType}'/${account}'/${direction}`,
+  );
+  referenceKey = directionRoot.deriveChild(index);
+  let wif3 = await b58c.encode({
+    privateKey: referenceKey.privateKey.toString("hex"),
+    compressed: true,
+  });
+
+  let accountRoot = privateRoot.derive(`m/44'/${coinType}'/${account}'`);
+  directionRoot = accountRoot.deriveChild(direction);
+  referenceKey = directionRoot.deriveChild(index);
+  let wif4 = await b58c.encode({
+    privateKey: referenceKey.privateKey.toString("hex"),
+    compressed: true,
+  });
+
+  /*
+   *     seed.deriveChild(44, true)
+   *         .deriveChild(5, true)
+   *         .deriveChild(0, true)
+   *         .deriveChild(0)
+   *         .deriveChild(0)
+   *
+   * Or, in other words:
+   *     let walletTypeRoot = seed.deriveChild(44, true);
+   *     let coinTypeRoot = walletTypeRoot.deriveChild(5, true);
+   *     let accountRoot = coinTypeRoot.deriveChild(0, true);
+   *     let directionRoot = accountRoot.deriveChild(0);
+   *     let key = directionRoot.deriveChild(0);
+   */
+
+  [wif2, wif3, wif4].forEach(function (wif) {
+    if (wif !== wif1) {
+      throw new Error(
+        `derived wif '${wif}' does not match other derived wif '${wif1}'`,
+      );
+    }
+  });
+  console.info(`WIF: ${wif1}`);
 }
 
 main()
   .then(function () {
-    console.info();
     process.exit(0);
   })
   .catch(function (err) {
-    console.error();
+    console.error(err.stack || err);
     process.exit(1);
   });
