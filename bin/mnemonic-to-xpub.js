c0c2f6d880f8d2f0316774a51b0a618a68ea1e67
AJ ONeal <coolaj86@gmail.com>
2022-12-18 00:57:22 -0700

    feat: add mnemonic-to-xprv and mnemonic-to-xpub

---
+++ bin/mnemonic-to-xpub.js
@@ -0,0 +1,99 @@
+#!/usr/bin/env node
+"use strict";
+
+let Fs = require("node:fs/promises");
+
+let Base58Check = require("@dashincubator/base58check").Base58Check;
+let b58c = Base58Check.create();
+let HdKey = require("hdkey");
+
+let Bip39 = require("bip39");
+
+let purpose = 44; // TODO testnet?
+let coinType = 5; // TODO testnet?
+
+async function main() {
+  let args = process.argv.slice(2);
+
+  let [mnemonicPath, passPath, hdpath] = args;
+  if (!mnemonicPath) {
+    console.error(
+      [
+        "",
+        "Usage",
+        "        mnemonic-to-xpub <./mnemonic.txt> [./password.txt] [hdpath]",
+        "",
+        "Examples",
+        "        # '/'-suffixed paths will assume the default prefix",
+        "        # (these are all equivalent)",
+        '        mnemonic-to-xpub ./mnemonic.txt ./password.txt "/0"',
+        '        mnemonic-to-xpub ./mnemonic.txt ./password.txt "/0\'/0"',
+        "        mnemonic-to-xpub ./mnemonic.txt ./password.txt \"m/44'/5'/0'/0\"",
+        "",
+        "        # 'm'-prefixed paths will be used exactly (allowing broad xkeys)",
+        "        # (each of these are different)",
+        "        mnemonic-to-xpub ./mnemonic.txt ./password.txt \"m/44'/5'/0'/0\"",
+        "        mnemonic-to-xpub ./mnemonic.txt ./password.txt \"m/44'/5'/0'\"",
+        "        mnemonic-to-xpub ./mnemonic.txt ./password.txt \"m/44'/5'\"",
+        "",
+        "IMPORTANT",
+        "        \"m/44'/5'\" MUST be used for compatibility with other Dash wallets",
+        "",
+      ].join("\n"),
+    );
+    process.exit(1);
+  }
+  let txt = await Fs.readFile(mnemonicPath, "utf8");
+  let words = txt
+    .trim()
+    .split(/[\s,]+/m)
+    .filter(Boolean);
+  let mnemonic = words.join(" ");
+
+  let secret = "";
+  if (passPath) {
+    let secretTxt = await Fs.readFile(passPath, "utf8");
+    secret = secretTxt.trim();
+  }
+
+  //let seedBuf = await Bip39.mnemonicToSeed(mnemonic);
+  let seedBuf = await Bip39.mnemonicToSeed(mnemonic, secret);
+
+  let privateRoot = HdKey.fromMasterSeed(seedBuf);
+
+  let defaultEntries = `m/${purpose}'/${coinType}'/0'/0`.split("/");
+
+  let fromEntries = (hdpath || "").split("/").filter(Boolean);
+
+  let isPathSuffix = !fromEntries[0]?.startsWith("m");
+  if (isPathSuffix) {
+    fromEntries = defaultEntries
+      .slice(0, defaultEntries.length - fromEntries.length)
+      .concat(fromEntries);
+  }
+
+  // a "wallet" is the path up to `m/44'/${coinType}'/${account}'`
+  // the "addresses" are the paths down from `${direction}/${index}`
+  let hdPrefix = fromEntries.join("/");
+  let xKey = privateRoot.derive(hdPrefix);
+  let xKeys = xKey.toJSON();
+
+  let xpubParts = await b58c.decode(xKeys.xpub);
+  let xpub = await b58c.encode(xpubParts);
+  if (xpub !== xKeys.xpub) {
+    throw new Error(
+      `@dashincubator/base58check gave '${xpub}', but expected '${xKeys.xpub}'`,
+    );
+  }
+
+  console.info(`${xpub}`);
+}
+
+main()
+  .then(function () {
+    process.exit(0);
+  })
+  .catch(function (err) {
+    console.error(err.stack || err);
+    process.exit(1);
+  });
