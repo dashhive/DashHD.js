15d9ccbf6250fb5974ba854cc94b34667c0dcf00
AJ ONeal <coolaj86@gmail.com>
2023-02-04 01:50:18 -0700

    WIP: BIG REFACTOR

--- dashhd.js
+++ dashhd.js
@@ -1,8 +1,7 @@
 /**
  * @typedef DashHD
  * @prop {HDCreate} create
- * @prop {HDFromSeed} fromMasterSeed
- * @prop {HDFromXKey} fromExtendedKey
+ * @prop {HDFingerprint} fingerprint
  * @prop {HDUtils} utils
  * @prop {Number} HARDENED_OFFSET - 0x80000000
  */
@@ -12,6 +11,7 @@
  * @prop {HDKeyTweak} privateKeyTweakAdd
  * @prop {HDKeyTweak} publicKeyTweakAdd
  * @prop {HDKeyToKey} publicKeyNormalize
+ * @prop {ReadUInt32BE} readUInt32BE
  * @prop {HDHasher} ripemd160sum
  * @prop {HDHasher} sha256sum
  * @prop {HDHasher} sha512hmac
@@ -35,14 +35,13 @@
  * @prop {HDVersions} versions - magic bytes for base58 prefix
  * @prop {HDDerivePath} derive - derive a full hd path from the given root
  * @prop {HDDeriveChild} deriveChild - get the next child xkey (in a path segment)
- * @prop {HDFingerprint} getFingerprint
- * @prop {HDMaybeGetString} getPrivateExtendedKey
- * @prop {HDMaybeGetBuffer} getPrivateKey
- * @prop {HDGetString} getPublicExtendedKey
- * @prop {HDSetBuffer} setPublicKey
- * @prop {HDSetBuffer} setPrivateKey
+ * @prop {HDMaybeGetString} xprvDigest
+ * @prop {HDMaybeGetBuffer} getPrivKey
+ * @prop {HDGetString} xpubDigest
+ * @prop {HDFromXKey} setXPrv
+ * @prop {HDFromXKey} setXPub
+ * @prop {HDSetSeed} setSeed
  * @prop {HDWipePrivates} wipePrivateData - randomizes private key buffer in-place
- * @prop {Function} _setPublicKey
  */
 
 /** @type {DashHD} */
@@ -128,6 +127,14 @@ var DashHd = ("object" === typeof module && exports) || {};
     return Q.toRawBytes(COMPRESSED);
   };
 
+  /** @type {ReadUInt32BE} */
+  Utils.readUInt32BE = function (u8, offset) {
+    let dv = new DataView(u8.buffer);
+    // will read offset + 4 bytes (32-bit uint)
+    let n = dv.getUint32(offset, BUFFER_BE);
+    return n;
+  };
+
   /** @type {HDHasher} */
   Utils.ripemd160sum = async function (bytes) {
     //@ts-ignore
@@ -194,7 +201,7 @@ var DashHd = ("object" === typeof module && exports) || {};
     return Secp256k1.getPublicKey(privBytes, COMPRESSED);
   };
 
-  // "Bitcoin seed"
+  // "Bitcoin seed" as per spec, regardless of network
   let MASTER_SECRET = Uint8Array.from([
     // B     i     t     c     o     i     n   " "     s     e     e     d
     0x42, 0x69, 0x74, 0x63, 0x6f, 0x69, 0x6e, 0x20, 0x73, 0x65, 0x65, 0x64,
@@ -210,67 +217,22 @@ var DashHd = ("object" === typeof module && exports) || {};
   DashHd.create = function (versions) {
     /** @type {hdkey} */
     let hdkey = {};
-    /** @type {Uint8Array?} */
-    let _privateKey = null;
+
+    hdkey.getPrivKey = function () {
+      return null;
+    };
 
     hdkey.versions = versions || BITCOIN_VERSIONS;
+
     hdkey.depth = 0;
-    hdkey.index = 0;
-    //hdkey.publicKey = null;
-    //hdkey.identifier = null;
-    //hdkey.chainCode = null;
     hdkey.parentFingerprint = 0;
+    hdkey.index = 0;
+    //hdkey.chainCode = null;
 
-    hdkey.getFingerprint = function () {
-      if (!hdkey.identifier) {
-        throw new Error("Public key has not been set");
-      }
-      let i32be = readUInt32BE(hdkey.identifier, 0);
-      return i32be;
-    };
+    //hdkey.publicKey = null;
 
-    /**
-     * @param {Uint8Array} u8 - a "web" JS buffer
-     * @param {Number} offset - where to start reading
-     * @returns {Number} - a 0-shifted (uint) JS Number
-     */
-    function readUInt32BE(u8, offset) {
-      let dv = new DataView(u8.buffer);
-      // will read offset + 4 bytes (32-bit uint)
-      let n = dv.getUint32(offset, BUFFER_BE);
-      return n;
-    }
-
-    hdkey.getPrivateKey = function () {
-      return _privateKey;
-    };
-    hdkey.setPrivateKey = async function (value) {
-      assert(value.length === 32, "Private key must be 32 bytes.");
-
-      _privateKey = value;
-      hdkey.publicKey = await Utils.toPublicKey(value);
-      hdkey.identifier = await hash160(hdkey.publicKey);
-    };
-
-    hdkey.setPublicKey = async function (value) {
-      assert(
-        value.length === 33 || value.length === 65,
-        "Public key must be 33 or 65 bytes.",
-      );
-      let publicKey = await Utils.publicKeyNormalize(value);
-      await hdkey._setPublicKey(publicKey);
-    };
-
-    /**
-     * @param {Uint8Array} publicKey
-     */
-    hdkey._setPublicKey = async function (publicKey) {
-      hdkey.publicKey = publicKey;
-      hdkey.identifier = await hash160(publicKey);
-      _privateKey = null;
-    };
-
-    hdkey.getPrivateExtendedKey = async function () {
+    hdkey.xprvDigest = async function () {
+      let _privateKey = hdkey.getPrivKey();
       if (!_privateKey) {
         return null;
       }
@@ -278,47 +240,59 @@ var DashHd = ("object" === typeof module && exports) || {};
       let key = new Uint8Array(KEY_SIZE);
       key.set([0], 0);
       key.set(_privateKey, 1);
-      //@ts-ignore - wth?
-      return await Utils.encodeXPrv(
-        serialize(hdkey, 0 /*hdkey.versions.private*/, key),
-      );
+      let xprvRaw = serialize(hdkey, key);
+
+      return await Utils.encodeXPrv(xprvRaw);
     };
 
-    hdkey.getPublicExtendedKey = async function () {
+    hdkey.xpubDigest = async function () {
       if (!hdkey.publicKey) {
         throw new Error("Missing public key");
       }
 
-      return await Utils.encodeXPub(
-        serialize(hdkey, 0 /*hdkey.versions.public*/, hdkey.publicKey),
-      );
+      let xpubRaw = serialize(hdkey, hdkey.publicKey);
+      return await Utils.encodeXPub(xpubRaw);
     };
 
     hdkey.derive = async function (path) {
-      if (path === "m" || path === "M" || path === "m'" || path === "M'") {
-        return hdkey;
-      }
+      let childKey = await DashHd.derive(hdkey, path);
+      return childKey;
+    };
 
-      let entries = path.split("/");
-      let _hdkey = hdkey;
-      for (let i = 0; i < entries.length; i += 1) {
-        let c = entries[i];
-        if (i === 0) {
-          assert(/^[mM]{1}/.test(c), 'Path must start with "m" or "M"');
-          continue;
+    // have hardened key
+    // -> have private key
+    //    -> can derive private key
+    //    -> can derive public key
+    // -> NO private key
+    //    -> ERROR
+    // have UN-hardened key
+    // -> have private key
+    //    -> can derive private key
+    //    -> can derive public key
+    // -> NO private key
+    //    -> can derive public key
+
+    hdkey.deriveChild = async function (index, harden) {
+      for (;;) {
+        try {
+          //@ts-ignore
+          return await hdkey._deriveChild(index);
+        } catch (e) {
+          // In essence:
+          // if it couldn't produce a public key, just go on the next one
+          //
+          // More precisely:
+          //
+          // throw if IL >= n || (privateKey + IL) === 0
+          // In case parse256(IL) >= n or ki == 0,
+          // one should proceed with the next value for i
+
+          // throw if IL >= n || (g**IL + publicKey) is infinity
+          // In case parse256(IL) >= n or Ki is the point at infinity,
+          // one should proceed with the next value for i
+          index += 1;
         }
-
-        let hardened = c.length > 1 && c[c.length - 1] === "'";
-        let childIndex = parseInt(c, 10); // & (HARDENED_OFFSET - 1)
-        assert(childIndex < HARDENED_OFFSET, "Invalid index");
-        if (hardened) {
-          childIndex += HARDENED_OFFSET;
-        }
-
-        _hdkey = await _hdkey.deriveChild(childIndex);
       }
-
-      return _hdkey;
     };
 
     // IMPORTANT: never allow `await` (or other async) between writing to
@@ -327,104 +301,136 @@ var DashHd = ("object" === typeof module && exports) || {};
     let _indexBuffer = new Uint8Array(4);
     let _indexDv = new DataView(_indexBuffer.buffer);
 
-    hdkey.deriveChild = async function (index) {
-      let isHardened = index >= HARDENED_OFFSET;
+    //@ts-ignore
+    hdkey._deriveChild = async function (index, harden) {
       let offset = 0;
       _indexDv.setUint32(offset, index, BUFFER_BE);
 
       let data = new Uint8Array(INDEXED_KEY_SIZE);
-
-      if (isHardened) {
-        // Hardened child
+      let _privateKey = hdkey.getPrivKey();
+      if (harden) {
         if (!_privateKey) {
           throw new Error("Could not derive hardened child key");
         }
-
-        // data = 0x00 || ser256(kpar) || ser32(index)
-        data.set([0], 0); // 1
-        data.set(_privateKey, 1); // 32
-        data.set(_indexBuffer, KEY_SIZE);
+        index += HARDENED_OFFSET;
+        // 1 recover byte (null) + 32 Priv bytes
+        data.set([0], 0);
+        data.set(_privateKey, 1);
       } else {
-        // Normal child
-        // data = serP(point(kpar)) || ser32(index)
-        //      = serP(Kpar) || ser32(index)
+        // 1 recover byte + 32 Pub X bytes
         data.set(hdkey.publicKey, 0);
-        data.set(_indexBuffer, KEY_SIZE);
       }
+      data.set(_indexBuffer, KEY_SIZE);
 
-      let IBuf = await Utils.sha512hmac(hdkey.chainCode, data);
-      let I = new Uint8Array(IBuf);
+      let I = await Utils.sha512hmac(hdkey.chainCode, data);
       let IL = I.slice(0, 32);
       let IR = I.slice(32);
 
       let hd = DashHd.create(hdkey.versions);
 
+      /** @type {Uint8Array?} */
+      let nextPrivKey = null;
+
       // Private parent key -> private child key
       if (_privateKey) {
-        // ki = parse256(IL) + kpar (mod n)
-        try {
-          let privateKeyCopy = new Uint8Array(_privateKey);
-          await hd.setPrivateKey(
-            await Utils.privateKeyTweakAdd(privateKeyCopy, IL),
-          );
-          // throw if IL >= n || (privateKey + IL) === 0
-        } catch (err) {
-          // In case parse256(IL) >= n or ki == 0, one should proceed with the next value for i
-          return await hdkey.deriveChild(index + 1);
-        }
-        // Public parent key -> public child key
-      } else {
-        // Ki = point(parse256(IL)) + Kpar
-        //    = G*IL + Kpar
-        try {
-          let publicKeyCopy = new Uint8Array(hdkey.publicKey);
-          await hd.setPublicKey(
-            await Utils.publicKeyTweakAdd(publicKeyCopy, IL),
-          );
-          // throw if IL >= n || (g**IL + publicKey) is infinity
-        } catch (err) {
-          // In case parse256(IL) >= n or Ki is the point at infinity, one should proceed with the next value for i
-          return await hdkey.deriveChild(index + 1);
-        }
+        nextPrivKey = await Utils.privateKeyTweakAdd(_privateKey, IL);
+        hd.publicKey = await Utils.toPublicKey(nextPrivKey);
+      }
+      // Public parent key -> public child key
+      else {
+        hd.publicKey = await Utils.publicKeyTweakAdd(hd.publicKey, IL);
       }
 
-      hd.chainCode = IR;
+      hd.getPrivKey = function () {
+        return nextPrivKey;
+      };
+
+      // setting in XKey byte-order for consistency
       hd.depth = hdkey.depth + 1;
-      hd.parentFingerprint = hdkey.getFingerprint();
+      hd.parentFingerprint = await DashKeys.fingerprint(hdkey.publicKey);
       hd.index = index;
+      hd.chainCode = IR;
 
       return hd;
     };
 
+    /** @type {HDSetSeed} */
+    hdkey.setSeed = async function (seedBytes) {
+      let I = await Utils.sha512hmac(MASTER_SECRET, seedBytes);
+      let IL = I.subarray(0, 32);
+      let IR = I.subarray(32);
+
+      hdkey.chainCode = IR;
+      hdkey.getPrivKey = function () {
+        return IL;
+      };
+      hdkey.publicKey = await Utils.toPublicKey(IL);
+    };
+
     hdkey.wipePrivateData = function () {
+      let _privateKey = hdkey.getPrivKey();
       if (_privateKey) {
         Utils.secureErase(_privateKey);
       }
-      _privateKey = null;
+      hdkey.getPrivKey = function () {
+        return null;
+      };
       return hdkey;
     };
 
     return hdkey;
   };
 
-  DashHd.fromMasterSeed = async function (seedBuffer, versions) {
-    let IBuf = await Utils.sha512hmac(MASTER_SECRET, seedBuffer);
-    let I = new Uint8Array(IBuf);
-    let IL = I.subarray(0, 32);
-    let IR = I.subarray(32);
+  DashKeys.derive = async function (hdkey, path) {
+    if (path === "m" || path === "M" || path === "m'" || path === "M'") {
+      return hdkey;
+    }
 
-    let hdkey = DashHd.create(versions);
-    hdkey.chainCode = IR;
-    await hdkey.setPrivateKey(IL);
+    let entries = path.split("/");
+    for (let i = 0; i < entries.length; i += 1) {
+      let c = entries[i];
+      if (i === 0) {
+        assert(/^[mM]{1}/.test(c), 'Path must start with "m" or "M"');
+        continue;
+      }
+
+      let harden = c.length > 1 && c[c.length - 1] === "'";
+      let childIndex = parseInt(c, 10); // & (HARDENED_OFFSET - 1)
+      assert(childIndex < HARDENED_OFFSET, "Invalid index");
+
+      hdkey = await hdkey.deriveChild(childIndex, harden);
+    }
 
     return hdkey;
   };
 
-  DashHd.fromExtendedKey = async function (
-    base58key,
-    versions,
-    skipVerification,
-  ) {
+  DashHd.fingerprint = async function (pubBytes) {
+    if (!pubBytes) {
+      throw new Error("Public key has not been set");
+    }
+
+    /*
+     * Note: this *happens* to use the same algorithm
+     * as many toPkh() implementations but, semantically,
+     * this is NOT toPkh() - it has a different purpose.
+     * Furthermore, fingerprint() may change independently of toPkh().
+     */
+    let sha = await Utils.sha256sum(pubBytes);
+    let identifier = await Utils.ripemd160sum(sha);
+    let i32be = Utils.readUInt32BE(identifier, 0);
+    return i32be;
+  };
+
+  DashHd.xprvRecover = async function (xprv) {
+    // TODO check "xprv"
+    return await DashHd._setXKey(xprv);
+  };
+  DashHd.xpubRecover = async function (xpub) {
+    // TODO check "xpub"
+    return await DashHd._setXKey(xpub);
+  };
+
+  DashHd._setXKey = async function (base58key, versions, skipVerification) {
     // => version(4) || depth(1) || fingerprint(4) || index(4) || chain(32) || key(33)
     versions = versions || BITCOIN_VERSIONS;
     skipVerification = skipVerification || false;
@@ -456,17 +462,28 @@ var DashHd = ("object" === typeof module && exports) || {};
         version === versions.private,
         "Version mismatch: version does not match private",
       );
-      await hdkey.setPrivateKey(key.subarray(1)); // cut off first 0x0 byte
+
+      let IL = key.subarray(1); // cut off first 0x0 byte
+      hdkey.getPrivKey = function () {
+        return IL;
+      };
+      hdkey.publicKey = await Utils.toPublicKey(IL);
     } else {
       assert(
         version === versions.public,
         "Version mismatch: version does not match public",
       );
-      if (skipVerification) {
-        await hdkey._setPublicKey(key);
-      } else {
-        await hdkey.setPublicKey(key);
+      assert(
+        key.length === 33 || key.length === 65,
+        "Public key must be 33 or 65 bytes.",
+      );
+      if (!skipVerification) {
+        key = await Utils.publicKeyNormalize(key);
       }
+      hdkey.publicKey = key;
+      hdkey.getPrivKey = function () {
+        return null;
+      };
     }
 
     return hdkey;
@@ -484,38 +501,23 @@ var DashHd = ("object" === typeof module && exports) || {};
 
   /**
    * @param {hdkey} hdkey - TODO attach to hdkey
-   * @param {Number} version
    * @param {Uint8Array} key
    */
-  function serialize(hdkey, version, key) {
+  function serialize(hdkey, key) {
     // => version(4) || depth(1) || fingerprint(4) || index(4) || chain(32) || key(33)
-    let myOffset = -4;
-    let xkey = new Uint8Array(XKEY_SIZE + myOffset);
+    let xkey = new Uint8Array(XKEY_SIZE - 4);
     let xkeyDv = new DataView(xkey.buffer);
 
-    if (version) {
-      myOffset += 4;
-      xkeyDv.setUint32(myOffset, version, BUFFER_BE);
-    }
-    xkeyDv.setUint8(myOffset + 4, hdkey.depth);
+    xkeyDv.setUint8(0, hdkey.depth);
 
     let fingerprint = (hdkey.depth && hdkey.parentFingerprint) || 0x00000000;
-    xkeyDv.setUint32(myOffset + 5, fingerprint, BUFFER_BE);
-    xkeyDv.setUint32(myOffset + 9, hdkey.index, BUFFER_BE);
+    xkeyDv.setUint32(1, fingerprint, BUFFER_BE);
+    xkeyDv.setUint32(5, hdkey.index, BUFFER_BE);
 
-    xkey.set(hdkey.chainCode, myOffset + 13);
-    xkey.set(key, myOffset + 45);
+    xkey.set(hdkey.chainCode, 9);
+    xkey.set(key, 41);
 
     return xkey;
-  }
-
-  /**
-   * @param {Uint8Array} buf
-   * @returns {Promise<Uint8Array>}
-   */
-  async function hash160(buf) {
-    let sha = await Utils.sha256sum(buf);
-    return await Utils.ripemd160sum(sha);
   }
 
   DashHd.HARDENED_OFFSET = HARDENED_OFFSET;
@@ -548,7 +550,8 @@ if ("object" === typeof module) {
 
 /**
  * @callback HDFingerprint
- * @returns {Number}
+ * @param {Uint8Array} pubBytes - Public Key
+ * @returns {Promise<Number>} - 32-bit Big Endian integer
  */
 
 /**
@@ -557,13 +560,6 @@ if ("object" === typeof module) {
  * @param {HDVersions} [versions]
  * @param {Boolean} [skipVerification]
  * returns {Promise<hdkey>}
- */
-
-/**
- * @callback HDFromSeed
- * @param {Uint8Array} seedBuffer
- * @param {HDVersions} [versions]
- * @returns {Promise<hdkey>}
  */
 
 /**
@@ -624,5 +620,18 @@ if ("object" === typeof module) {
  */
 
 /**
+ * @callback HDSetSeed
+ * @param {Uint8Array} seedBytes
+ * @returns {Promise<void>}
+ */
+
+/**
  * @callback HDWipePrivates
+ */
+
+/**
+ * @callback ReadUInt32BE
+ * @param {Uint8Array} u8 - a "web" JS buffer
+ * @param {Number} offset - where to start reading
+ * @returns {Number} - a 0-shifted (uint) JS Number
  */
