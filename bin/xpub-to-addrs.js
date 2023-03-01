19a33814821986596bf2d6c8b115e9669e3ce930
AJ ONeal <coolaj86@gmail.com>
2023-03-01 14:27:05 -0700

    f: dashhd

--- bin/xpub-to-addrs.js
+++ bin/xpub-to-addrs.js
@@ -3,12 +3,7 @@
 
 let Fs = require("node:fs/promises");
 
-let Xaddr = require("./_xaddr.js");
-
-let Base58Check = require("@dashincubator/base58check").Base58Check;
-let HdKey = require("hdkey");
-
-let b58c = Base58Check.create();
+let DashHd = require("dashhd");
 
 async function main() {
   let args = process.argv.slice(2);
@@ -29,28 +24,25 @@ async function main() {
 
   let txt = await Fs.readFile(xpubPath, "utf8");
   let xpub = txt.trim();
-  let publicRoot = HdKey.fromExtendedKey(xpub);
-
-  let xpubParts = await b58c.decode(xpub);
-  let depth = parseInt(xpubParts.xpub.slice(0, 2), 16);
-  let parentIndex = parseInt(xpubParts.xpub.slice(10, 18), 16);
+  let xKey = await DashHd.fromXKey(xpub);
 
   let hdpath = "m";
-  for (let i = 1; i < depth; i += 1) {
+  for (let i = 1; i < xKey.depth; i += 1) {
     hdpath += "/?";
   }
   let harden = "";
   //@ts-ignore
-  let isHardened = parentIndex >= HdKey.HARDENED_OFFSET;
+  let isHardened = xKey.index >= DashHd.HARDENED_OFFSET;
   if (isHardened) {
     harden = "'";
   }
-  hdpath += `/${parentIndex}${harden}`;
+  hdpath += `/${xKey.index}${harden}`;
 
   for (let index = startIndex; index <= endIndex; index += 1) {
     //@ts-ignore
-    let hdPub = publicRoot.deriveChild(index);
-    let addr = await Xaddr.publicKeyToAddr(hdPub.publicKey);
+    let hdPub = await DashHd.deriveChild(xKey, index, isHardened);
+    //@ts-ignore - TODO optional opts
+    let addr = await DashHd.toAddr(hdPub.publicKey, {});
     let fullpath = `${hdpath}/${index}`;
 
     if (hasMany) {
